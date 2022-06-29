52c5ffddf53a341b242ed12afdcfd3b0a0e87201
shailesh2323 <shailesh2323.patil@gmail.com>
2022-06-29 18:30:46 +0530

    initial connut

--- app/src/main/java/com/example/myapplication/ResourceComparator.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-import android.content.Context
-
-class ResourceComparator {
-    fun isEqual(context: Context, resId: Int, string: String) : Boolean {
-        return context.getString(resId) == string
-    }
-}
